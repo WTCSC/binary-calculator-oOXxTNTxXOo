b4755d47c1c43792bbed24e68959e19161991d7e
oOXxTNTxXOo <bob.bross10@gmail.com>
2025-01-08 10:58:48 -0700

    setup

--- calculator.py
+++ calculator.py
@@ -1,2 +1,16 @@
+import operator
+ops = {
+    "+": operator.add,
+    "-": operator.sub,
+    "*": operator.mul,
+    "/": operator.div
+}
+
 def binary_calculator(bin1, bin2, operator):
-    # your code here
+
+    op_func = ops[operator]
+    binn =  op_func(bin1,bin2)
+    return binn
+
+"""100 find the power of the 1 and convert"""
+print("11111111 = 255 8-bit")
